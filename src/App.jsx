33e4965372e23440181ab732ffcdb4d79684796e
abdelkarim <aoulallayabdelkarim@gmail.com>
2023-04-02 21:39:18 +0000

    fixing detail page 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import { Layout } from "./components/Layout";
